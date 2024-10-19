6a106f6fceae35a259b97321015f3d938862a3b4
amxmodx <admin@hiosu.com>
2024-10-19 15:57:00 +0800

    如果队伍只有一个玩家，flag使用玩家头像

--- osu.Game.Tournament/Components/DrawableTeamFlag.cs
+++ osu.Game.Tournament/Components/DrawableTeamFlag.cs
@@ -31,19 +31,40 @@ namespace osu.Game.Tournament.Components
         private void load(TextureStore textures)
         {
             if (team == null) return;
-
-            Size = new Vector2(75, 54);
             Masking = true;
             CornerRadius = 5;
-            Child = flagSprite = new Sprite
-            {
-                RelativeSizeAxes = Axes.Both,
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
-                FillMode = FillMode.Fill
-            };
 
-            (flag = team.FlagName.GetBoundCopy()).BindValueChanged(_ => flagSprite.Texture = textures.Get($@"Flags/{team.FlagName}"), true);
+            if (team.Players.Count == 1)
+            {
+
+                Size = new Vector2(75, 75);
+
+                int uID = team.Players[0].OnlineID;
+                string avatarUrl = $@"https://a.ppy.sh/{uID}";
+
+                Child = new Sprite
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    FillMode = FillMode.Fit,
+                    Texture = textures.Get(avatarUrl),
+                };
+            }
+            else
+            {
+                Size = new Vector2(75, 54);
+
+                Child = flagSprite = new Sprite
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    FillMode = FillMode.Fill  // ÆìÖÄÊÊÓ¦ÈÝÆ÷´óÐ¡
+                };
+
+                (flag = team.FlagName.GetBoundCopy()).BindValueChanged(_ => flagSprite.Texture = textures.Get($@"Flags/{team.FlagName}"), true);
+            }
         }
     }
 }
